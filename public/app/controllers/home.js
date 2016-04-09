8540ca148664a9a0720af66162f1d9308907830d
Katrina Rodriguez <karodriguez8@gmail.com>
2016-04-09 10:12:44 -0400

    update design

--- public/app/controllers/home.js
+++ public/app/controllers/home.js
@@ -3,52 +3,46 @@ var homeCtr = angular.module('HomeModule', []);
 homeCtr.controller('HomeController', ['$scope', function($scope){
 
 	$scope.profile = {
-		name: 'Rafael Nadal',
-		city: 'Madrid',
-		job: 'Tennis Stahh'
+		name: '',
+		email: '',
+		phone: '',
+		message: ''
 	}
 
 
 	$scope.people = [
-		{name: 'hillary clinton', city: 'new york', job: 'secretary of state'},
-		{name: 'bernie sanders', city: 'vermont', job: 'congressman'},
-		{name: 'michael bloomberg', city: 'new york', job: 'millionaire'}
+		{name: 'hillary clinton', email: 'new york', phone: 'secretary of state', message: ''},
+		{name: 'bernie sanders', email: 'vermont', phone: 'congressman', message: ''},
+		{name: 'michael bloomberg', email: 'new york', phone: 'millionaire', message: ''}
 	]
 
-	$scope.visitor = {
-		name: '',
-		email: '',
-		phone: ''
+
+
+	$scope.addProfile = function(){
+		console.log('AddProfile: '+JSON.stringify($scope.profile));
+		$scope.people.push($scope.profile);
 	}
-
-	$scope.cities = [
-		'san francisco',
-		'austin',
-		'barcelona',
-		'berlin',
-		'bogota'
-	]
 
 	$scope.init = function(){
 		console.log('HomeController: INIT');
 	}
 
 	$scope.submit = function(){
-		if ($scope.visitor.name.length == 0) {
+		if ($scope.profile.name.length == 0) {
 			alert('Oops, need more info to continue. Please enter your name.');
 			return;
 		}
 
-		if ($scope.visitor.email.length == 0) {
+		if ($scope.profile.email.length == 0) {
 			alert('Oops, need more info to continue. Please enter your email.');
 			return;
 		}
 		
-		if ($scope.visitor.phone.length == 0) {
+		if ($scope.profile.phone.length == 0) {
 			alert('Oops, need more info to continue. Please enter your phone number.');
 			return;
 		}
 
-		console.log('HomeController: SUBMIT' + JSON.stringify($scope.visitor))
+		console.log('HomeController: SUBMIT' + JSON.stringify($scope.profile))
 	}
 }]);
